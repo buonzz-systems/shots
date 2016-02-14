4d274faa10a3828a093dd10464592022be6126b3
buonzz <buonzz@gmail.com>
2016-02-14 16:41:11 +0000

    created command and config file

---
+++ config/shots.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'cameras' => [
+        array('hostname'=> '192.168.1.104', 
+              'username' => 'user', 
+              'password' => 'password',
+              'port' => 81)
+    ],
+    'ftp_servers' => [
+        array('hostname'=> '192.168.1.104', 
+              'username' => 'user', 
+              'password' => 'password',
+              'path' => '/')
+    ]
+];
